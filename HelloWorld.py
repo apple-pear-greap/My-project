265383c5145bcbdded154633cd656de3562b341f
Hysilens <Hysilens@csu.edu.cn>
2025-10-08 13:31:27 +0800

    Try to use pycharm

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,7 @@
-print('Hello World')
-if True:
-    print('Hello World')
-if False:
-    print('goodbye')
+name = input("What is your name? ")
+print('Hello ',name ,'I\'m Cerydra'
+                     'You\'re',name)
+
 
 #试试看能不能正常更新
 #试试看加速器关掉能不能正常上传
